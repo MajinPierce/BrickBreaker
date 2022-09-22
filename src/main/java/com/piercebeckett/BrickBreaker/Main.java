6576b7e3ae9b2939e2709e92fef45ff5b9705eec
PierceBeckett <pierceabeckett@gmail.com>
2022-09-22 17:57:27 -0500

    brick hit detection and removal except it's weird and broken

--- src/main/java/com/piercebeckett/BrickBreaker/Main.java
+++ src/main/java/com/piercebeckett/BrickBreaker/Main.java
@@ -16,68 +16,21 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main extends Application{
 
     private static Paddle paddle = new Paddle();
     private static Ball ball = new Ball();
     private double paddleDx = 0;
+    private ArrayList<Brick> bricks = new ArrayList<>();
+    private Iterator<Brick> bricksI = bricks.iterator();
 
     public static final double WINDOW_WIDTH = 640;
     public static final double WINDOW_HEIGHT = 480;
     public static final double epsilon = 1;
     static final int NUMBER_OF_ROWS = 5;
     static final double BRICK_GAP = 2;
-
-    AnimationTimer timer = new AnimationTimer() {
-        @Override
-        public void handle(long currentNanoTime) {
-            //double t = (currentNanoTime - startNanoTime) / 1000000000.0;
-            //double x = ((640 - paddle.getWidth()) / 2) * Math.sin(t) + ((640- paddle.getWidth())    /2);
-            //paddle.setX(x);
-
-            //paddle window edge detection
-            paddle.setX(paddle.getX() + paddleDx);
-            if(paddle.getX() < 0){
-                paddle.setX(0);
-            }
-            else if(paddle.getX() > (WINDOW_WIDTH - paddle.getWidth())){
-                paddle.setX(WINDOW_WIDTH - paddle.getWidth());
-            }
-
-            //update ball position
-            ball.setCenterX(ball.getCenterX() + ball.getxVelocity());
-            ball.setCenterY(ball.getCenterY() + ball.getyVelocity());
-
-            //ball vertical window edge detection
-            if((ball.getCenterX() - ball.getRadius()) < 0) {
-                ball.setxVelocity(ball.getxVelocity() * -1);
-                ball.setCenterX(ball.getRadius());
-            }
-            else if((ball.getCenterX() + ball.getRadius()) > WINDOW_WIDTH){
-                ball.setxVelocity(ball.getxVelocity() * -1);
-                ball.setCenterX(WINDOW_WIDTH - ball.getRadius());
-            }
-
-            //ball horizontal window edge and paddle detection
-            if((ball.getCenterY() - ball.getRadius()) < 0) {
-                ball.setyVelocity(ball.getyVelocity() * -1);
-                ball.setCenterY(ball.getRadius());
-            }
-            else if((ball.getCenterY() + ball.getRadius()) > WINDOW_HEIGHT){
-                ball.setyVelocity(ball.getyVelocity() * -1);
-                ball.setCenterY(WINDOW_HEIGHT - ball.getRadius());
-            }
-            else if(almostEqual((ball.getCenterY() + ball.getRadius()), paddle.getY())
-            && ((ball.getCenterX() > paddle.getX()) &&
-                    (ball.getCenterX() < (paddle.getX() + paddle.getWidth()))))
-            {
-                ball.setyVelocity(ball.getyVelocity() * -1);
-                ball.setCenterY(paddle.getY() - ball.getRadius());
-                ball.setxVelocity(calculateBallPaddleVelocity());
-            }
-        }
-    };
 
     @Override
     public void start(Stage stage) {
@@ -96,7 +49,7 @@ public class Main extends Application{
 
         root.getChildren().addAll(paddle, ball);
 
-        ArrayList<Brick> bricks = new ArrayList<>();
+        //initialize grid of bricks
         int columns = (int) Math.floor(WINDOW_WIDTH / (Brick.BASE_WIDTH + BRICK_GAP));
         System.out.println("columns: " + columns);
         double buffer = (WINDOW_WIDTH - ((Brick.BASE_WIDTH + BRICK_GAP) * columns)) + 2*BRICK_GAP;
@@ -117,6 +70,143 @@ public class Main extends Application{
             root.getChildren().add(brick);
         }
 
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long currentNanoTime) {
+                //double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                //double x = ((640 - paddle.getWidth()) / 2) * Math.sin(t) + ((640- paddle.getWidth())    /2);
+                //paddle.setX(x);
+
+                //paddle window edge detection
+                paddle.setX(paddle.getX() + paddleDx);
+                if(paddle.getX() < 0){
+                    paddle.setX(0);
+                }
+                else if(paddle.getX() > (WINDOW_WIDTH - paddle.getWidth())){
+                    paddle.setX(WINDOW_WIDTH - paddle.getWidth());
+                }
+
+                //update ball position
+                ball.setCenterX(ball.getCenterX() + ball.getxVelocity());
+                ball.setCenterY(ball.getCenterY() + ball.getyVelocity());
+
+                //ball vertical window edge detection
+                if((ball.getCenterX() - ball.getRadius()) < 0) {
+                    ball.setxVelocity(ball.getxVelocity() * -1);
+                    ball.setCenterX(ball.getRadius());
+                }
+                else if((ball.getCenterX() + ball.getRadius()) > WINDOW_WIDTH){
+                    ball.setxVelocity(ball.getxVelocity() * -1);
+                    ball.setCenterX(WINDOW_WIDTH - ball.getRadius());
+                }
+
+                //ball horizontal window edge
+                if((ball.getCenterY() - ball.getRadius()) < 0) {
+                    ball.setyVelocity(ball.getyVelocity() * -1);
+                    ball.setCenterY(ball.getRadius());
+                }
+                else if((ball.getCenterY() + ball.getRadius()) > WINDOW_HEIGHT){
+                    ball.setyVelocity(ball.getyVelocity() * -1);
+                    ball.setCenterY(WINDOW_HEIGHT - ball.getRadius());
+                }
+
+                //paddle edge detection
+                if(almostEqual((ball.getCenterY() + ball.getRadius()), paddle.getY())
+                        && ((ball.getCenterX() > paddle.getX()) &&
+                        (ball.getCenterX() < (paddle.getX() + paddle.getWidth()))))
+                {
+                    ball.setyVelocity(ball.getyVelocity() * -1);
+                    ball.setCenterY(paddle.getY() - ball.getRadius());
+                    ball.setxVelocity(calculateBallPaddleVelocity());
+                }
+
+                //ball to brick hit detection
+                for(Brick brick : bricks) {
+                    //horizontal detection
+                    if (almostEqual((ball.getCenterY() - ball.getRadius()), (brick.getY() + brick.getHeight()))
+                            && ((ball.getCenterX() > brick.getX()) &&
+                            (ball.getCenterX() < (brick.getX() + brick.getWidth())))) {
+                        ball.setyVelocity(ball.getyVelocity() * -1);
+                        ball.setCenterY(brick.getY() + brick.getHeight() + ball.getRadius());
+                        brick.setHealth(brick.getHealth() - 1);
+                        if (brick.getHealth() == 0) {
+                            root.getChildren().remove(brick);
+                            bricks.remove(brick);
+                            break;
+                        }
+                    } else if (almostEqual((ball.getCenterY() + ball.getRadius()), (brick.getY()))
+                            && ((ball.getCenterX() > brick.getX()) &&
+                            (ball.getCenterX() < (brick.getX() + brick.getWidth())))) {
+                        ball.setyVelocity(ball.getyVelocity() * -1);
+                        ball.setCenterY(brick.getY() - ball.getRadius());
+                        brick.setHealth(brick.getHealth() - 1);
+                        if (brick.getHealth() == 0) {
+                            root.getChildren().remove(brick);
+                            bricks.remove(brick);
+                            break;
+                        }
+                    }
+                    //vertical detection
+                    if (almostEqual((ball.getCenterX() - ball.getRadius()), (brick.getX() + brick.getWidth()))
+                            && ((ball.getCenterY() > brick.getY()) &&
+                            (ball.getCenterY() < (brick.getY() + brick.getHeight())))) {
+                        ball.setyVelocity(ball.getxVelocity() * -1);
+                        ball.setCenterX(brick.getX() + brick.getWidth() + ball.getRadius());
+                        brick.setHealth(brick.getHealth() - 1);
+                        if (brick.getHealth() == 0) {
+                            root.getChildren().remove(brick);
+                            bricks.remove(brick);
+                            break;
+                        }
+                    } else if (almostEqual((ball.getCenterX() + ball.getRadius()), (brick.getX()))
+                            && ((ball.getCenterY() > brick.getY()) &&
+                            (ball.getCenterY() < (brick.getY() + brick.getHeight())))) {
+                        ball.setyVelocity(ball.getxVelocity() * -1);
+                        ball.setCenterX(brick.getX() - ball.getRadius());
+                        brick.setHealth(brick.getHealth() - 1);
+                        if (brick.getHealth() == 0) {
+                            root.getChildren().remove(brick);
+                            bricks.remove(brick);
+                            break;
+                        }
+                    }
+                }
+
+                /*
+                while(bricksI.hasNext()){
+                    Brick brick = bricksI.next();
+                    //horizontal detection
+                    if(almostEqual((ball.getCenterY() - ball.getRadius()), (brick.getY() + brick.getHeight()))
+                            && ((ball.getCenterX() > brick.getX()) &&
+                            (ball.getCenterX() < (brick.getX() + brick.getWidth()))))
+                    {
+                        ball.setyVelocity(ball.getyVelocity() * -1);
+                        ball.setCenterY(brick.getY() + brick.getHeight() + ball.getRadius());
+                        brick.setHealth(brick.getHealth() - 1);
+                        if(brick.getHealth() == 0) {
+                            root.getChildren().remove(brick);
+                            bricksI.remove();
+                        }
+                    }
+                    else if(almostEqual((ball.getCenterY() + ball.getRadius()), (brick.getY()))
+                            && ((ball.getCenterX() > brick.getX()) &&
+                            (ball.getCenterX() < (brick.getX() + brick.getWidth()))))
+                    {
+                        ball.setyVelocity(ball.getyVelocity() * -1);
+                        ball.setCenterY(brick.getY() - ball.getRadius());
+                        brick.setHealth(brick.getHealth() - 1);
+                        if(brick.getHealth() == 0) {
+                            root.getChildren().remove(brick);
+                            bricksI.remove();
+                        }
+                    }
+                    else{
+                        bricksI.next();
+                    }
+                }
+                */
+            }
+        };
         timer.start();
         stage.show();
     }
